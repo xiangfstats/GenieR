a9fa1f8d9f469c0cfe40545ae5ebf6a98f679053
Fei Xiang <xf3087@gmail.com>
2016-11-02 17:04:48 +0000

    new functions

---
+++ R/att.R
@@ -0,0 +1,48 @@
+#'Sort out sampling times, coalescent times and sampling lineages from a phylogenetic tree
+#'
+#' @export
+
+att <- function(phy,eps=1e-6){
+  b.s.times = branching.sampling.times(phy)
+  int.ind = which(as.numeric(names(b.s.times)) < 0)
+  tip.ind = which(as.numeric(names(b.s.times)) > 0)
+  num.tips = length(tip.ind)
+  num.coal.events = length(int.ind)
+  sampl.suf.stat = rep(NA, num.coal.events)
+  coal.interval = rep(NA, num.coal.events)
+  coal.lineages = rep(NA, num.coal.events)
+  sorted.coal.times = sort(b.s.times[int.ind])
+  names(sorted.coal.times) = NULL
+  sampling.times = sort((b.s.times[tip.ind]))
+  for (i in 2:length(sampling.times)){
+    if ((sampling.times[i]-sampling.times[i-1])<eps){
+      sampling.times[i]<-sampling.times[i-1]}
+  }
+  unique.sampling.times<-unique(sampling.times)
+  sampled.lineages = NULL
+  for (sample.time in unique.sampling.times){
+    sampled.lineages = c(sampled.lineages,
+                         sum(sampling.times == sample.time))
+  }
+  if(sum(sorted.coal.times %in% unique.sampling.times)>0){
+    stop("Simultaneous sample and coalescence time")
+  }
+  all.times <- sort(unique(c(unique.sampling.times,sorted.coal.times)))
+  # Check that first time is sampling
+  if(!(all.times[1] %in% unique.sampling.times)){
+    stop("Samples must be first (in reverse time)")
+  }
+  A <- rep(0,length(all.times))
+  lastA <- 0
+  for(i in 1:length(all.times)){
+    is.sample <- match(all.times[i],unique.sampling.times)
+    if(!is.na(is.sample)){
+      ss <- sampled.lineages[is.sample]
+      A[i] <- lastA + ss
+    }else{
+      A[i] <- lastA - 1
+    }
+    lastA <- A[i]
+  }
+  data.frame(t=all.times,A=A)
+}
