db1bbf8af3112bc7f5faefe20974f8ebdd40ffd6
Fei Xiang <xf3087@gmail.com>
2016-12-16 16:46:16 +0000

    deletion of cumsum

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,19 +5,6 @@
 
 using namespace Rcpp;
 
-// cumsum_bounded
-NumericVector cumsum_bounded(NumericVector x, double low, double high);
-RcppExport SEXP genieR_cumsum_bounded(SEXP xSEXP, SEXP lowSEXP, SEXP highSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type x(xSEXP);
-    Rcpp::traits::input_parameter< double >::type low(lowSEXP);
-    Rcpp::traits::input_parameter< double >::type high(highSEXP);
-    rcpp_result_gen = Rcpp::wrap(cumsum_bounded(x, low, high));
-    return rcpp_result_gen;
-END_RCPP
-}
 // negllc_const
 double negllc_const(double N0, NumericVector t, NumericVector A);
 RcppExport SEXP genieR_negllc_const(SEXP N0SEXP, SEXP tSEXP, SEXP ASEXP) {
